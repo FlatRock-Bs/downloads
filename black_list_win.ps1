5974c1157c38bae8fc509e76def354d0660922a8
Nikolay Grudov <117655120+FlatRock-Bs@users.noreply.github.com>
2024-04-01 23:09:41 +0300

    Update black_list_win.ps1

--- black_list_win.ps1
+++ black_list_win.ps1
@@ -3,9 +3,7 @@ $domains = @("example.com", "test.com")
 
 # Loop through each domain and add firewall rule to block it
 foreach ($domain in $domains) {
-    # Create a new inbound rule to block traffic to the domain over HTTP (port 80)
+    # Create a new inbound rule to block traffic to the domain over HTTP (port 80) and HTTPS (port 443)
     New-NetFirewallRule -DisplayName "Block $domain HTTP" -Direction Inbound -Action Block -Protocol TCP -RemoteAddress Any -LocalPort 80 -RemotePort Any -Description "Block traffic to $domain over HTTP"
-
-    # Create a new inbound rule to block traffic to the domain over HTTPS (port 443)
     New-NetFirewallRule -DisplayName "Block $domain HTTPS" -Direction Inbound -Action Block -Protocol TCP -RemoteAddress Any -LocalPort 443 -RemotePort Any -Description "Block traffic to $domain over HTTPS"
 }
